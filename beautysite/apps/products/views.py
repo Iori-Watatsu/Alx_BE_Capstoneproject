ab7c367fd23da53583f8d4e6cf43c899885a041c
Kamohelo <kamohelotshabalala@proton.me>
2025-12-09 01:32:17 +0200

    Change function names in all current apps views.py

--- beautysite/apps/products/views.py
+++ beautysite/apps/products/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def index(request):
+def products(request):
     return HttpResponse("Browse beauty products")
--- beautysite/apps/users/views.py
+++ beautysite/apps/users/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def index(request):
+def users(request):
     return HttpResponse("Welcome, User")
