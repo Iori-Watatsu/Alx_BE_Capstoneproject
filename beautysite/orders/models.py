2cecdb0e9336506282878e4c246056bae40fc20c
Kamohelo <kamohelotshabalala@proton.me>
2025-12-10 23:53:39 +0200

    Add oder models in orders/models.py

--- beautysite/orders/models.py
+++ beautysite/orders/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.db.models import TextField
+
 
 # Create your models here.
+class Order(models.Model):
+    id = models.IntegerField()
+    order_number = models.CharField(max_length=100)
+    user_id = models.IntegerField()
+    total_amount = models.DecimalField()
+    status = models.TextField()
+    shipping_address = models.TextField()
+    billing_address = models.TextField()
+    payment_method = models.CharField(max_length=100)
+    payment_status = models.CharField(max_length=100)
+    created_at = models.DateTimeField()
+    updated_at = models.DateTimeField()
