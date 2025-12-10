fa48bf543168fce154ba627d3e712546882e2e4e
Kamohelo <kamohelotshabalala@proton.me>
2025-12-11 00:45:57 +0200

    Add wishlist model in wishlist/models.py & check if check if other commits have been updated after fleet github token has been updated

--- beautysite/wishlist/models.py
+++ beautysite/wishlist/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Wishlist(models.Model):
+    id = models.IntegerField()
+    user_id = models.IntegerField()
+    product_id = models.IntegerField()
+    added_at = models.DateTimeField()
+    
