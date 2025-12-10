463612502e0440a0543681fd3666a06f02bef8c5
Kamohelo <kamohelotshabalala@proton.me>
2025-12-11 00:32:25 +0200

    Add user model in users/models.py

--- beautysite/users/models.py
+++ beautysite/users/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+id = models.IntegerField()
+usename = models.CharField(max_length=15)
+email = models.EmailField()
+password = models.CharField(min_length=16, max_length=50)
+first_name = models.TextField()
+last_name = models.TextField()
+phone_number = models.IntegerField()
+date_joined = models.DateTimeField()
+is_active = models.BooleanField()
+is_staff = models.BooleanField()
+is_superuser = models.BooleanField()
