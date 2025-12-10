da09f9e0f73a8a430b0679ed6f6b58ee0a577384
Kamohelo <kamohelotshabalala@proton.me>
2025-12-11 00:21:55 +0200

    Add review model in reviews.models.py

--- beautysite/reviews/models.py
+++ beautysite/reviews/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Review(models.Model):
+    id = models.IntegerField()
+    user_id = models.IntegerField()
+    product_id = models.IntegerField()
+    rating = models.IntegerField()
+    title = models.CharField(max_length=100)
+    comment = models.TextField()
+    is_approved = models.BooleanField()
+    created_at = models.DateTimeField()
