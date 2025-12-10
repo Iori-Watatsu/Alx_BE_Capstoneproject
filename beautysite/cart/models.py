7588c1afb78c8da142a8a055ee409f66d4d6d861
Kamohelo <kamohelotshabalala@proton.me>
2025-12-10 23:43:19 +0200

    Add cart model in cart/models.py

--- beautysite/cart/models.py
+++ beautysite/cart/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Cart(models.Model):
+    id = models.IntegerField()
+    user_id = models.IntegerField()
+    created_at = models.DateTimeField()
+    updated_at = models.DateTimeField()
