6663b1331516f7ea20771ebfeaec5aec191e9829
Kamohelo <kamohelotshabalala@proton.me>
2025-12-11 01:05:00 +0200

    Configure database in beautysite/settings.py

--- beautysite/beautysite/settings.py
+++ beautysite/beautysite/settings.py
@@ -83,8 +83,12 @@ WSGI_APPLICATION = 'beautysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': BASE_DIR / 'beautydatabase',
+        'USER': 'kamohelo',
+        'PASSWORD': 'beautypass',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
