8cd46514e2635fa6e8e9879267bcc00f57d545bc
Kamohelo <kamohelotshabalala@proton.me>
2025-12-11 02:38:57 +0200

    Update wishlist/models.py user_id & product_id with models.ForeignKey

--- beautysite/wishlist/models.py
+++ beautysite/wishlist/models.py
@@ -1,9 +1,11 @@
 from django.db import models
+from users.models import Profile
+from products.models import Product
 
 # Create your models here.
 class Wishlist(models.Model):
     id = models.IntegerField()
-    user_id = models.IntegerField()
-    product_id = models.IntegerField()
+    user_id = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='profile')
+    product_id = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='products')
     added_at = models.DateTimeField()
     
