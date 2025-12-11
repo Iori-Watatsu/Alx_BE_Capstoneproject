28a0eea064291ff22292998a9be58cb16a2b26ca
Kamohelo <kamohelotshabalala@proton.me>
2025-12-11 02:50:28 +0200

    Update products/models.py category_id models.ForeignKey & add Category model

--- beautysite/products/models.py
+++ beautysite/products/models.py
@@ -1,11 +1,12 @@
 from django.db import models
+from products.models import Category
 
 # Create your models here.
 class Product(models.Model):
     id = models.IntegerField()
     name = models.TextField()
     description = models.TextField()
-    category_id = models.IntegerField()
+    category_id = models.ForeignKey(Category, on_delete=models.CASCADE, related_name="categories")
     brand = models.CharField(max_length=100)
     price = models.DecimalField()
     sale_price = models.DecimalField()
@@ -18,7 +19,15 @@ class Product(models.Model):
 
 class Product_Image(models.Model):
     id = models.IntegerField()
-    product_id = models.ForeignKey(Product, related_name='products')
+    product_id = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='products')
     image_url = models.CharField(max_length=50)
     alt_text = models.TextField()
     is_primary = models.BooleanField()
+
+class Category(models.Model):
+    id = models.IntegerField()
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    slug = models.TextField()
+    parent_id = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='products')
+    is_active = models.BooleanField()
