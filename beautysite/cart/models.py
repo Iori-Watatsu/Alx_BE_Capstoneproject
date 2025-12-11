c1ba3ea2bc8af134b590bad7ab36d0a0bc0df142
Kamohelo <kamohelotshabalala@proton.me>
2025-12-11 02:15:11 +0200

    Update cart/models.py Cart_Items model product_id

--- beautysite/cart/models.py
+++ beautysite/cart/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from products.models import Product
 
 # Create your models here.
 class Cart(models.Model):
@@ -10,6 +11,6 @@ class Cart(models.Model):
 class Cart_Item(models.Model):
     id = models.IntegerField()
     cart_id = models.ForeignKey(Cart, on_delete=models.CASCADE, related_name='cart_items')
-    product_id = models.ForeignKey(Cart, on_delete=models.CASCADE, related_name='cart_items')
+    product_id = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='products')
     quantity = models.IntegerField()
     added_at = models.DateTimeField()
