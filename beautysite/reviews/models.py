81f6aed6a929642e4ca57d03747ef74c37ec5afa
Kamohelo <kamohelotshabalala@proton.me>
2025-12-11 02:34:30 +0200

    Update reviews/models.py user_id & product_id with models.ForeignKey

--- beautysite/reviews/models.py
+++ beautysite/reviews/models.py
@@ -1,10 +1,12 @@
 from django.db import models
+from users.models import User
+from products.models import Product
 
 # Create your models here.
 class Review(models.Model):
     id = models.IntegerField()
-    user_id = models.IntegerField()
-    product_id = models.IntegerField()
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE, related_name='profile')
+    product_id = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='products')
     rating = models.IntegerField()
     title = models.CharField(max_length=100)
     comment = models.TextField()
