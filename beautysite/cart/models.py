fb567fe79a8afc81fa6fd5a92599d6306273e246
Kamohelo <kamohelotshabalala@proton.me>
2025-12-11 02:05:57 +0200

    Update cart/models.py with Cart_Items model

--- beautysite/cart/models.py
+++ beautysite/cart/models.py
@@ -6,3 +6,10 @@ class Cart(models.Model):
     user_id = models.IntegerField()
     created_at = models.DateTimeField()
     updated_at = models.DateTimeField()
+
+class Cart_Item(models.Model):
+    id = models.IntegerField()
+    cart_id = models.ForeignKey(Cart, on_delete=models.CASCADE, related_name='cart_items')
+    product_id = models.ForeignKey(Cart, on_delete=models.CASCADE, related_name='cart_items')
+    quantity = models.IntegerField()
+    added_at = models.DateTimeField()
