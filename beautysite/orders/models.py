72399bd12105ed007aea0ad4df0be5b183f0cb7a
Kamohelo <kamohelotshabalala@proton.me>
2025-12-13 00:28:39 +0200

    Change import from Product to Products in orders/models,py

--- beautysite/orders/models.py
+++ beautysite/orders/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.db.models import TextField, CASCADE
-from products.models import Product
+from products.models import products
 from users.models import Profile
 
 # Create your models here.
