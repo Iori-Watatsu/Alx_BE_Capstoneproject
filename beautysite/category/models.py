c499824b72f51760ddce5e1b81ac898efba060a7
Kamohelo <kamohelotshabalala@proton.me>
2025-12-25 02:48:41 +0200

    Remove Category model in products/models.py & code model for category/models.py

--- beautysite/category/models.py
+++ beautysite/category/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    slug = models.SlugField(max_length=200, unique=True)
+    parent_id = models.ForeignKey('Product', on_delete=models.CASCADE, related_name='parent_categories', null =True, blank=True)
+    is_active = models.BooleanField(default=True)
