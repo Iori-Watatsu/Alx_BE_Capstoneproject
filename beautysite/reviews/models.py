f8a5032f0b848cc2c3a4aa0fedb67d8da51acfaf
Kamohelo <kamohelotshabalala@proton.me>
2025-12-25 08:47:39 +0200

    Change users.model import & user_id to CustomUser

--- beautysite/reviews/models.py
+++ beautysite/reviews/models.py
@@ -1,11 +1,11 @@
 from django.db import models
-from users.models import User
+from users.models import CustomUser
 from products.models import Product
 
 # Create your models here.
 class Review(models.Model):
     id = models.IntegerField()
-    user_id = models.ForeignKey(User, on_delete=models.CASCADE, related_name='profile')
+    user_id = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='profile')
     product_id = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='products')
     rating = models.IntegerField()
     title = models.CharField(max_length=100)
