898742ac6d4a35503538d58536a45cd813ab614d
Kamohelo <kamohelotshabalala@proton.me>
2025-12-25 11:23:24 +0200

    Comment out CustomerUser fields and update fields in users/models.py

--- beautysite/users/models.py
+++ beautysite/users/models.py
@@ -3,17 +3,17 @@ from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 class CustomUser(AbstractUser):
-    id = models.IntegerField()
-    username = models.CharField(max_length=15)
-    email = models.EmailField()
-    password = models.CharField(max_length=50)
-    first_name = models.TextField()
-    last_name = models.TextField()
-    phone_number = models.IntegerField()
-    date_joined = models.DateTimeField()
-    is_active = models.BooleanField()
-    is_staff = models.BooleanField()
-    is_superuser = models.BooleanField()
+    #id = models.IntegerField()
+    #username = models.CharField(max_length=15)
+    #email = models.EmailField()
+    #password = models.CharField(max_length=50)
+    #first_name = models.TextField()
+    #last_name = models.TextField()
+    phone_number = models.CharField(max_length=255, blank=True)
+    #date_joined = models.DateTimeField()
+    #is_active = models.BooleanField()
+    #is_staff = models.BooleanField()
+    #is_superuser = models.BooleanField()
 
 
 class Profile(models.Model):
@@ -21,7 +21,7 @@ class Profile(models.Model):
     user_id = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='profile')
     avatar_url = models.CharField(max_length=100)
     date_of_birth = models.DateField()
-    gender = models.CharField(max_length=10, blank=True, choices=[
+    gender = models.CharField(max_length=1, blank=True, choices=[
         ('M', 'Male'),
         ('F', 'Female'),
         ('O', 'Other'),
@@ -35,7 +35,7 @@ class Profile(models.Model):
     ])
     hair_type = models.CharField(max_length=50, blank=True)
     address = models.TextField()
-    city = models.TextField()
-    country = models.TextField()
-    postal_code = models.TextField()
-    created_at = models.DateTimeField()
+    city = models.CharField(max_length=100)
+    country = models.CharField(max_length=100)
+    postal_code = models.CharField(max_length=)
+    created_at = models.DateTimeField(auto_now=True)
