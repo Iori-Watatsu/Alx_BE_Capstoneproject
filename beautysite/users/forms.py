42728d10babd431effaf502aa738e144c4116945
Kamohelo <kamohelotshabalala@proton.me>
2025-12-25 04:26:51 +0200

    Initial commit of beautysite/users/forms.py

---
+++ beautysite/users/forms.py
@@ -0,0 +1,37 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from .models import CustomUser, UserProfile
+
+class CustomUserCreationForm(UserCreationForm):
+    email = forms.EmailField(required=True)
+    first_name = forms.CharField(required=False, max_length=30)
+    last_name = forms.CharField(required=False, max_length=30)
+
+    class Meta:
+        model = CustomUser
+        fields = ('username', 'email', 'first_name', 'last_name', 'password1', 'password2')
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.email = self.cleaned_data['email']
+        user.first_name = self.cleaned_data['first_name']
+        user.last_name = self.cleaned_data['last_name']
+
+        if commit:
+            user.save()
+        return user
+
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model = UserProfile
+        fields = ['skin_type', 'skin_concerns', 'preferred_brands', 'allergies', 'avatar']
+        widgets = {
+            'skin_concerns': forms.Textarea(attrs={'rows': 3}),
+            'preferred_brands': forms.Textarea(attrs={'rows': 3}),
+            'allergies': forms.Textarea(attrs={'rows': 3}),
+        }
+
+class UserUpdateForm(forms.ModelForm):
+    class Meta:
+        model = CustomUser
+        fields = ['first_name', 'last_name', 'email']
