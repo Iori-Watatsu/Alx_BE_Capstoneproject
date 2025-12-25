7d40651599298d4c405aa4033d8e50a9b927fead
Kamohelo <kamohelotshabalala@proton.me>
2025-12-25 11:27:41 +0200

    Add settings import & update Review fields in reviews/models.py

--- beautysite/reviews/models.py
+++ beautysite/reviews/models.py
@@ -1,14 +1,16 @@
 from django.db import models
 from users.models import CustomUser
 from products.models import Product
+from django.conf import settings
 
 # Create your models here.
 class Review(models.Model):
     id = models.IntegerField()
-    user_id = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='profile')
+    user_id = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='profile')
     product_id = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='products')
     rating = models.IntegerField()
     title = models.CharField(max_length=100)
     comment = models.TextField()
     is_approved = models.BooleanField()
-    created_at = models.DateTimeField()
+    created_at = models.DateTimeField(auto_now=True)
+    content = models.TextField()
