d579f9d16a4489180bd6fc912f89e076f92ed82b
Kamohelo <kamohelotshabalala@proton.me>
2025-12-25 04:32:24 +0200

    Update paths in beautysite/users/urls.py

--- beautysite/users/urls.py
+++ beautysite/users/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    path("", views.users, name="users"),
-    path('templates/'),
-    path('wishlist/', views.WishlistView.as_view(), name='wishlist'),
+    path("", views.home, name="home"),
+    path('signup/', views.SignUpView.as_view(), name='signup'),
+    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('profile/', views.profile, name='profile'),
+    path('profile/edit/', views.edit_profile, name='edit_profile'),
 ]
