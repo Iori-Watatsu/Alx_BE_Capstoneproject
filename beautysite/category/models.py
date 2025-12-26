4cf7ede24bbe5a2ba5fb25d00aa71ca19394d071
Kamohelo <kamohelotshabalala@proton.me>
2025-12-26 03:52:21 +0200

    Remove product field in Category app models

--- beautysite/category/models.py
+++ beautysite/category/models.py
@@ -8,4 +8,3 @@ class Category(models.Model):
     slug = models.SlugField(max_length=200, unique=True)
     parent = models.ForeignKey('self', on_delete=models.CASCADE, related_name='children', null =True, blank=True)
     is_active = models.BooleanField(default=True)
-    product = models.ForeignKey('self', null=True, blank=True, on_delete=models.CASCADE, related_name='children')
