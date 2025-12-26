32b9ee703bf0a7353cdfa0c5ae79f335922ac6ff
Kamohelo <kamohelotshabalala@proton.me>
2025-12-26 03:27:35 +0200

    Update parent_id to psretn and code in category/models.py

--- beautysite/category/models.py
+++ beautysite/category/models.py
@@ -6,6 +6,6 @@ class Category(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
     slug = models.SlugField(max_length=200, unique=True)
-    parent_id = models.ForeignKey('Product', on_delete=models.CASCADE, related_name='subcategories', null =True, blank=True)
+    parent = models.ForeignKey('self', on_delete=models.CASCADE, related_name='children', null =True, blank=True)
     is_active = models.BooleanField(default=True)
     product = models.ForeignKey('self', null=True, blank=True, on_delete=models.CASCADE, related_name='children')
