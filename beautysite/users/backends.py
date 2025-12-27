5dcc5c78848b25af6de157952dcc147323d307e9
Kamohelo <kamohelotshabalala@proton.me>
2025-12-28 01:37:12 +0200

    Create custom backend in users

---
+++ beautysite/users/backends.py
@@ -0,0 +1,26 @@
+from django.contrib.auth.backends import BaseBackend
+
+user = get_user_model()
+
+class EmailBAckend(BaseBackend):
+    def authenticate(self, request, username=None, password=None):
+        if username is None or password is None:
+            return None
+
+        try:
+            user = user.objects.get(email__iexact=username)
+        except user.DoesNotExits:
+            return None
+
+        if user.check_password(password) and self.user_can_authenticate(user):
+            return user
+        return None
+
+    def get_user(selfself, user_id):
+        try:
+            return user.objects.get(pk=user_id)
+        except user.DoesNotExits:
+            return None
+
+    def user_can_authenticate(selfself, user):
+        return getattr(user, "is_active", True)
