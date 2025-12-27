fc9e99695d56a15b0481e861bac5835a2c764036
Kamohelo <kamohelotshabalala@proton.me>
2025-12-28 01:51:42 +0200

    Correct User model name in users/backends.py

--- beautysite/users/backends.py
+++ beautysite/users/backends.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.backends import BaseBackend
 from django.contrib.auth import get_user_model
 
-user = get_user_model()
+User = get_user_model()
 
 class EmailBackend(BaseBackend):
     def authenticate(self, request, username=None, password=None):
