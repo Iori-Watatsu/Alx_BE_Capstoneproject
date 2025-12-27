d44429460a7323072ebca6dc9c5cb9472fca5118
Kamohelo <kamohelotshabalala@proton.me>
2025-12-27 18:18:20 +0200

    Cahnge category_id to category in products/models.py

--- beautysite/products/models.py
+++ beautysite/products/models.py
@@ -8,7 +8,7 @@ class Product(models.Model):
     
     name = models.CharField(max_length=255)
     description = models.TextField()
-    category_id = models.ForeignKey(Category, on_delete=models.CASCADE, related_name="products")
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name="products")
     brand = models.CharField(max_length=100)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     sale_price = models.DecimalField(max_digits=10, decimal_places=2)
