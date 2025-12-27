79495bad6764a440e59860ba0e5bd5461cb8b4d8
Kamohelo <kamohelotshabalala@proton.me>
2025-12-28 01:11:56 +0200

    Import SignupView in beautysite/urls.py

--- beautysite/beautysite/urls.py
+++ beautysite/beautysite/urls.py
@@ -17,6 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 from django.views.generic import TemplateView
+from users.views import SignupView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
