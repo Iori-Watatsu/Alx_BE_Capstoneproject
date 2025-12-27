22b9b7aef24375b3faeb5a5caadb125a34300b9f
Kamohelo <kamohelotshabalala@proton.me>
2025-12-28 01:23:48 +0200

    Uncomment CustomUser fields in users/models.py

--- beautysite/users/models.py
+++ beautysite/users/models.py
@@ -5,16 +5,16 @@ from django.conf import settings
 # Create your models here.
 class CustomUser(AbstractUser):
 
-    #username = models.CharField(max_length=15)
-    #email = models.EmailField()
-    #password = models.CharField(max_length=50)
-    #first_name = models.TextField()
-    #last_name = models.TextField()
+    username = models.CharField(max_length=150, unique=True)
+    email = models.EmailField()
+    password = models.CharField(max_length=50)
+    first_name = models.TextField()
+    last_name = models.TextField()
     phone_number = models.CharField(max_length=255, blank=True)
-    #date_joined = models.DateTimeField()
-    #is_active = models.BooleanField()
-    #is_staff = models.BooleanField()
-    #is_superuser = models.BooleanField()
+    date_joined = models.DateTimeField()
+    is_active = models.BooleanField()
+    is_staff = models.BooleanField()
+    is_superuser = models.BooleanField()
 
 
 class Profile(models.Model):
