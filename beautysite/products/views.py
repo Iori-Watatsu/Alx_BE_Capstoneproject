713837f9db96381d026ef7f9f55bfdda529f908f
Kamohelo <kamohelotshabalala@proton.me>
2025-12-29 03:17:03 +0200

    Fix Product.object typo in products/views.py for migrations bug fix

--- beautysite/products/views.py
+++ beautysite/products/views.py
@@ -14,7 +14,7 @@ class IsAdminOrReadOnly(permissions.BasePermission):
         return request.user and request.user.is_staff
 
 class ProductViewSet(viewsets.ModelViewSet):
-    queryset = Product.object.filter(is_active=True)
+    queryset = Product.objects.filter(is_active=True)
     serializer_class = ProductSerializer
     permission_classes = [IsAdminOrReadOnly]
 
