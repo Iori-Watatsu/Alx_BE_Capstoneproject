73687ccca5254d4dd73683f12e4d3146a983f4a8
Kamohelo <kamohelotshabalala@proton.me>
2026-01-03 18:10:15 +0200

    Update ordering, filtering and searching views in products/views.py

--- beautysite/products/views.py
+++ beautysite/products/views.py
@@ -1,9 +1,11 @@
 from django.http import HttpResponse
-from rest_framework import viewsets, permissions
+from rest_framework import viewsets, permissions, filters
+from django_filters.rest_framework import DjangoFilterBackend
 from .models import Product
 from .serializers import ProductSerializer
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.viewsets import ModelViewSet
+from .filters import ProductFilter
 
 # Create your views here.
 def products(request):
@@ -16,10 +18,17 @@ class IsAdminOrReadOnly(permissions.BasePermission):
         return request.user and request.user.is_staff
 
 class ProductViewSet(ModelViewSet):
-    queryset = Product.objects.filter(is_active=True)
+    queryset = Product.objects.all(is_active=True)
     serializer_class = ProductSerializer
     permission_classes = [IsAuthenticated]
 
-    filterset_fields = ['brand']
+    filter_backends = [
+        DjangoFilterBackend,
+        filters.SearchFilter,
+        filters.OrderingFilter,
+    ]
+
+    filterset_fields = ProductFilter
     search_fields = ['name', 'description']
-    ordering_fields = ['price', 'created_at']
+    ordering_fields = ['price', 'created_at', 'price', 'name']
+    ordering = ['created_at']
