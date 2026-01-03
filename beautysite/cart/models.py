feb8cf80f8c6189cb4f778b251fc43d4dc4a7a4a
Kamohelo <kamohelotshabalala@proton.me>
2026-01-03 16:46:28 +0200

    Add post model in cart/models.py

--- beautysite/cart/models.py
+++ beautysite/cart/models.py
@@ -28,3 +28,16 @@ class Cart_Item(models.Model):
 
     class Meta:
         unique_together = ('user', 'product')
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="posts"
+    )
+    created_at = models.DateTimeField(auto_now_add==True)
+
+    def __str__(self):
+        return self.title
