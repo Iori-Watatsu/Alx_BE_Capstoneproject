8cca06a39c02de4c362093a83ed25187550d89c8
Kamohelo <kamohelotshabalala@proton.me>
2026-01-03 17:03:24 +0200

    Add post views in cart/views.py

--- beautysite/cart/views.py
+++ beautysite/cart/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from rest_framework import generics
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
+from .models import Post
+from serializers import PostSerializer
+from .permissions import IsAuthorOrReadOnly
 
 # Create your views here.
 def cart(request):
@@ -8,3 +14,20 @@ def cart(request):
 def perform_create(self, serializer):
     serializer.save(user=self.request.user)
     
+class PostListCreateAPIView(generics.ListCreateAPIView):
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+    serializer_class = [PostSerializer]
+
+    def get_queryset(self):
+        return Post.objects.filter(author=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+class PostReviewUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+    serializer_class = [PostSerializer]
+    queryset = Post.objects.all()
+
