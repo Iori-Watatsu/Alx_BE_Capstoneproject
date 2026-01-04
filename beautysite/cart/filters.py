90d54e698289e91aeb338a726551862faaf9504a
Kamohelo <kamohelotshabalala@proton.me>
2026-01-04 16:58:52 +0200

    Update cart models and filters for testing

--- beautysite/cart/filters.py
+++ beautysite/cart/filters.py
@@ -1,10 +1,10 @@
 import django_filters
-from .models import Cart_Item
+from .models import CartItem
 
 class CartItemFilter(django_filters.FilterSet):
 
     class Meta:
-        model = Cart_Item
+        model = CartItem
         fieldsets = {
             'product_name':['icontains'],
             'quantity':['gte', 'lte']
--- beautysite/cart/models.py
+++ beautysite/cart/models.py
@@ -14,7 +14,7 @@ class Cart(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now_add=True)
 
-class Cart_Item(models.Model):
+class CartItem(models.Model):
     user = models.ForeignKey(
         settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='user_cart'
     )
