823bfb428d80cfe5cbb00a937046df901294fe7d
Kamohelo <kamohelotshabalala@proton.me>
2026-01-04 08:17:04 +0200

    Fix auto_now_add typo for migrations in cart/models.py

--- beautysite/cart/models.py
+++ beautysite/cart/models.py
@@ -37,7 +37,7 @@ class Post(models.Model):
         on_delete=models.CASCADE,
         related_name="posts"
     )
-    created_at = models.DateTimeField(auto_now_add==True)
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.title
