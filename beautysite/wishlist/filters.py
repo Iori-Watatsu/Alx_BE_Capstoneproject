c8b7cb0b7d681c114f951ee74571bdebfec9fe3d
Kamohelo <kamohelotshabalala@proton.me>
2026-01-04 16:49:13 +0200

    Update wishlist filters for testing

--- beautysite/wishlist/filters.py
+++ beautysite/wishlist/filters.py
@@ -2,8 +2,9 @@ import django_filters
 from .models import Wishlist
 
 class WishlistFilter(django_filters.FilterSet):
+
     class Meta:
         model = Wishlist
         fields = {
-            'product_name': ['icotains']
+            'product': ['icontains']
         }
