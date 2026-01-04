321b2319c9447ac0c2d2cec0123e124bb778fac9
Kamohelo <kamohelotshabalala@proton.me>
2026-01-04 17:02:18 +0200

    Update filters.py in wishlist

--- beautysite/wishlist/filters.py
+++ beautysite/wishlist/filters.py
@@ -2,9 +2,8 @@ import django_filters
 from .models import Wishlist
 
 class WishlistFilter(django_filters.FilterSet):
+    product = django_filters.CharFilter(field_name='product', lookup_expr='icontains')
 
     class Meta:
         model = Wishlist
-        fields = {
-            'product': ['icontains']
-        }
+        fields = ['product']
