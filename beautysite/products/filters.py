a2bedeb7bc51b87161001cfb45ff2a8a6e66b43f
Kamohelo <kamohelotshabalala@proton.me>
2026-01-04 08:23:36 +0200

    Add s in fields typo for products/filters.py

--- beautysite/products/filters.py
+++ beautysite/products/filters.py
@@ -6,7 +6,7 @@ class ProductFilter(django_filters.FilterSet):
     class Meta:
         model = Product
         fields = {
-            'name':['icontain'],
+            'name':['icontains'],
             'price':['gte', 'lte'],
             'category':['exact'],
             'brand':['exact'],
