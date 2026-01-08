eb78b05c2a46f93669d5b9cd2d47c106ac2e747d
Kamohelo <kamohelotshabalala@proton.me>
2026-01-08 04:28:31 +0200

    Add viewset in category/views.py

--- beautysite/category/views.py
+++ beautysite/category/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from .models import Category
+from products.serializers import CategorySerializer
 
 # Create your views here.
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
