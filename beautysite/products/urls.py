e1c15bd91f14f2e17d7fc6c92807dae528cf9c3b
Kamohelo <kamohelotshabalala@proton.me>
2026-01-08 03:45:08 +0200

    Add categories router in products/urls.py

--- beautysite/products/urls.py
+++ beautysite/products/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import ProductViewSet
+from .views import ProductViewSet, CategoryViewSet
 
 router = DefaultRouter()
-router.register('products', ProductViewSet, basename='products')
+router.register(r'products', ProductViewSet, basename='product')
+router.register(r'categories', ProductViewSet, basename='category')
+
 
 urlpatterns = [
     path('api', include(router.urls)),
