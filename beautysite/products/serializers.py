810d76fd6b7693e1b35fa3a24712ab0220e1a3ff
Kamohelo <kamohelotshabalala@proton.me>
2026-01-08 03:37:59 +0200

    Update products/serializers.py w/ CategorySerializer

--- beautysite/products/serializers.py
+++ beautysite/products/serializers.py
@@ -1,13 +1,27 @@
 from rest_framework import serializers
-from .models import Product
+from .models import Product, Category
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = '__all__'
 
 class ProductSerializer(serializers.ModelSerializer):
     is_on_sale = serializers.SerializerMethodField()
+    category = CategorySerializer(read_only=True)
+    category_id = serializers.IntegerField(write_only=True, required=False)
 
     class Meta:
         model = Product
-        fields = '__all__'
-        read_only_fields = ('id', 'created_at')
+        fields = [
+            'id', 'name', 'description', 'price', 'sale_price',
+            'stock', 'in_stock', 'category', 'category_id',
+            'created_at', 'updated_at' 
+        ]
+        read_only_fields = [
+            'id', 'created_at', 'updated_at','in_stock'
+        ]
 
     def validate_price(self, value):
         if value <= 0 : # \eqslantless sign was automatically created when adding the = sign next to the <
@@ -23,4 +37,5 @@ class ProductSerializer(serializers.ModelSerializer):
 
     def get_is_on_sale(self, obj):
         return obj.sale_price < obj.price
+
 
