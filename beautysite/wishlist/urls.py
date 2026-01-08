450cd248f3b51d30dddf30250c16a05a2bda14d5
Kamohelo <kamohelotshabalala@proton.me>
2026-01-08 05:18:10 +0200

    Add posts urls in wishlist/urls.py

--- beautysite/wishlist/urls.py
+++ beautysite/wishlist/urls.py
@@ -1,10 +1,13 @@
 from django.conf.urls.i18n import urlpatterns
 from django.db import router
-from django.urls import path
+from django.urls import path, include
 from .views import (PostListCreateAPIView, PostRetrieveUpdateDestroyAPIView, WishlistViewSet)
 from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
 router.register(r'', WishlistViewSet, basename='wishlist')
 
-urlpatterns = router.urls
+urlpatterns = [
+    path('', include(router.urls)),
+    path('posts/', PostListCreateAPIView.as_view(), name='post-list'),
+    path('posts/<int:pk>/', PostRetrieveUpdateDestroyAPIView.as_view(), name='post-detail'),
