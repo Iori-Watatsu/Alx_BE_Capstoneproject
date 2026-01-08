c94732a2f10655feb40477fdbdb1aff0e83fdb30
Kamohelo <kamohelotshabalala@proton.me>
2026-01-08 05:10:17 +0200

    Change fields to all in orders/serializers.py

--- beautysite/orders/serializers.py
+++ beautysite/orders/serializers.py
@@ -9,13 +9,7 @@ class PostSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Post
-        fields = [
-            'id',
-            'title',
-            'content',
-            'author_username',
-            'created_at',
-        ]
+        fields = '__all__'
 
 
 class OrderSerializer(serializers.ModelSerializer):
