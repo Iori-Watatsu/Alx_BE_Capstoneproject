6fbe19c85c49b3c2fe95752073f267bf960ac2da
Kamohelo <kamohelotshabalala@proton.me>
2026-01-08 05:07:40 +0200

    Remove attributes for all posts urls in orders/urls.py

--- beautysite/orders/urls.py
+++ beautysite/orders/urls.py
@@ -8,6 +8,6 @@ router.register(r'', OrderViewSet, basename='order')
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('posts/', PostListCreateAPIView.as_view({'get': 'list', 'post': 'create'}), name='post-list'),
-    path('posts/<int:pk>/', PostRetrieveUpdateDestroyAPIView.as_view({'get': 'retrieve', 'put': 'update', 'patch': 'partial_update', 'delete': 'destroy'}), name='post-detail'),
+    path('posts/', PostListCreateAPIView.as_view(sd), name='post-list'),
+    path('posts/<int:pk>/', PostRetrieveUpdateDestroyAPIView.as_view(), name='post-detail'),
 ]
