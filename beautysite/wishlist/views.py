89032e9f87b9132202058c3bb13694dac20d4152
Kamohelo <kamohelotshabalala@proton.me>
2026-01-08 04:21:55 +0200

    Update wishlist/views.py and add PostViewSet

--- beautysite/wishlist/views.py
+++ beautysite/wishlist/views.py
@@ -1,8 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from rest_framework import generics, viewsets, filters
+from rest_framework import generics, viewsets, filters, permissions
 from rest_framework.authentication import TokenAuthentication
-from rest_framework.permissions import IsAuthenticated
 from .models import Post, Wishlist
 from .serializers import PostSerializer, WishlistSerializer
 from .permissions import IsAuthorOrReadOnly
@@ -10,12 +9,9 @@ from .filters import WishlistFilter
 from django_filters.rest_framework import DjangoFilterBackend
 
 # Create your views here.
-def wishlist(request):
-    return HttpResponse("Your wishes")
-
 class PostListCreateAPIView(generics.ListCreateAPIView):
     authentication_classes = [TokenAuthentication]
-    permission_classes = [IsAuthenticated]
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     serializer_class = PostSerializer
 
     def get_queryset(self):
@@ -29,13 +25,14 @@ class PostRetrieveUpdateDestroyAPIView(
     generics.RetrieveUpdateDestroyAPIView
 ):
     authentication_classes = [TokenAuthentication]
-    permission_classes = [IsAuthenticated, IsAuthorOrReadOnly]
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsAuthorOrReadOnly]
     queryset = Post.objects.all()
     serializer_class = PostSerializer
 
 class WishlistViewSet(viewsets.ModelViewSet):
+    queryset = Wishlist.objects.all()
     serializer_class = WishlistSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
     filter_backends = [
         DjangoFilterBackend,
@@ -49,3 +46,11 @@ class WishlistViewSet(viewsets.ModelViewSet):
 
     def get_queryset(self):
         return Wishlist.objects.filter(user=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
